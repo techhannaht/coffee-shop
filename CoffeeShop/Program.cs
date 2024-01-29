4177410183e6ea136a3d645e10b5d7074bad9c1b
Hannah Thompson <hannahmthompsonedu@gmail.com>
2024-01-29 13:46:08 -0500

    coffee shop back end

---
+++ CoffeeShop/Program.cs
@@ -0,0 +1,49 @@
+
+using CoffeeShop.Repositories;
+
+namespace CoffeeShop
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddTransient<IBeanVarietyRepository, BeanVarietyRepository>();
+            builder.Services.AddTransient<ICoffeeRepository, CoffeeRepository>();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CoffeeShop v1"));
+
+                // Do not block requests while in development
+                app.UseCors(options =>
+                {
+                    options.AllowAnyOrigin();
+                    options.AllowAnyMethod();
+                    options.AllowAnyHeader();
+                });
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+
+            app.Run();
+        }
+    }
+}
